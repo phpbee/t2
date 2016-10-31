dd676c7ac78822cf18905b6d8c45b4c47d9384e3
alex <alex@kochetov.com>
2016-10-31 16:43:52 +0300

    GET /t2/hello

---
+++ src/main/java/org/phpbee/t2/login/LoginController.java
@@ -0,0 +1,18 @@
+package org.phpbee.t2.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+
+    private static final String HELLO_VIEW = "hello";
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(Model model) {
+        model.addAttribute("subject", "world");
+        return HELLO_VIEW;
+    }
+}
